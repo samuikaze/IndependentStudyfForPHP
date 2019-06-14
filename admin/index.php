8e0cd750c3fc8e890a9820fdaf5fe77d1689ed6e
SamuiKaze <rr5145@gmail.com>
2019-06-14 11:34:23 +0800

    modify favicon

--- admin/index.php
+++ admin/index.php
@@ -30,6 +30,7 @@ if(!empty($_GET['action']) && $_GET['action'] == 'backendlogout'){
 <meta http-equiv="Cache-Control" content="no-store" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="shortcut icon" href="../images/favicon.ico" />
 <script type="text/javascript" src="../js/jquery-2.2.3.min.js"></script>
 <script type="text/javascript" src="../js/jquery-ui.min.js"></script>
 <script type="text/javascript" src="../js/i18n/datepicker-zh-TW.js"></script>
